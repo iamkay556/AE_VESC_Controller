8ea702ed34dd06b8c47161d375cd89650df3989e
kay singhal <130114612+iamkay556@users.noreply.github.com>
2025-04-12 22:42:07 -0700

    added relay on/off switches

--- src/main.cpp
+++ src/main.cpp
@@ -10,25 +10,32 @@ ezButton reverseSwitch(53);
 
 String mode = "none";
 
-// Pin to read Analog input from linear potentiometer
-int readPIN = A0;
+#define readPIN A0       // pot pin
+#define SignalBattery 22 // signal from buck to activate battery relay
+#define SignalSolar 23   // signal from buck to activate solar relay
 
-/** Initiate VescUart class */
+#define relayBattery 24 // output pin to control battery relay
+#define relaySolar 25   // output pin to control solar relay
+
 VescUart UART;
 
-LiquidCrystal_I2C lcd(0x27, 15,4);
+LiquidCrystal_I2C lcd(0x27, 15, 4);
 
-void setup() {
+void setup()
+{
 
   lcd.init();
   lcd.backlight();
-  /** Setup Serial port to display data */
+
   Serial.begin(9600);
 
   /** Setup UART port (Serial1 on Atmega32u4) */
   Serial1.begin(115200);
-  
-  while (!Serial) {;}
+
+  while (!Serial)
+  {
+    ;
+  }
 
   /** Define which ports to use as UART */
   UART.setSerialPort(&Serial1);
@@ -40,6 +47,16 @@ void setup() {
 
   pinMode(readPIN, INPUT);
 
+  pinMode(SignalBattery, INPUT);
+  pinMode(SignalSolar, INPUT);
+
+  pinMode(relayBattery, OUTPUT);
+  pinMode(relaySolar, OUTPUT);
+
+  // rekays start in off mode
+  digitalWrite(relayBattery, LOW);
+  digitalWrite(relaySolar, LOW);
+
   driveSwitch.setDebounceTime(10);
   neutralSwitch.setDebounceTime(10);
   reverseSwitch.setDebounceTime(10);
@@ -47,30 +64,66 @@ void setup() {
   delay(1000);
 }
 
-void dutyset(float start, float end) {
+void dutyset(float start, float end)
+{
   const float step = 0.04;
   const float tolerance = 0.03;
 
-  while (abs(start - end) > tolerance) {
-    if (end > start) {
+  while (abs(start - end) > tolerance)
+  {
+    if (end > start)
+    {
       start += step;
-      if (start > end) start = end;
-    } else if (end < start) {
+      if (start > end)
+        start = end;
+    }
+    else if (end < start)
+    {
       start -= step;
-      if (start < end) start = end;
+      if (start < end)
+        start = end;
     }
 
-    if (mode == "DRIVE") UART.setDuty(start);
-    else if (mode == "REVERSE") UART.setDuty(-start);
-    else if (mode == "NEUTRAL") UART.setDuty(-start);
-    else Serial.println("dutyset() func not working b/c no mode");
+    if (mode == "DRIVE")
+      UART.setDuty(start);
+    else if (mode == "REVERSE")
+      UART.setDuty(-start);
+    else if (mode == "NEUTRAL")
+      UART.setDuty(-start);
+    else
+      Serial.println("dutyset() func not working b/c no mode");
 
     delay(10);
- 
   }
 }
 
-void loop() {
+void loop()
+{ // --------------------------------- LOOP ---------------------------------
+
+  int batonoff = digitalRead(SignalBattery);
+  int solonoff = digitalRead(SignalSolar);
+
+  if (batonoff == 1)
+  {
+    digitalWrite(relayBattery, HIGH);
+    Serial.println("BATTERY ON");
+  }
+  else
+  {
+    digitalWrite(relayBattery, LOW);
+    Serial.println("BATTERY OFF");
+  }
+  if (solonoff == 1)
+  {
+    digitalWrite(relaySolar, HIGH);
+    Serial.println("SOLAR ON");
+  }
+  else
+  {
+    digitalWrite(relaySolar, LOW);
+    Serial.println("SOLAR OFF");
+  }
+
   driveSwitch.loop();
   neutralSwitch.loop();
   reverseSwitch.loop();
@@ -78,39 +131,43 @@ void loop() {
   int revst = reverseSwitch.getState();
   int neust = neutralSwitch.getState();
   int drist = driveSwitch.getState();
-  
+
   /** Read The Input from the petentiometer and map the values from 0 - 1*/
   float pot = analogRead(readPIN);
-  pot = (pot/1023) * 1.0;
+  pot = (pot / 1023) * 1.0;
   Serial.println(pot);
 
-  if (drist == 0) {
+  if (drist == 0)
+  {
     mode = "DRIVE";
     Serial.println("Mode swithched to : DRIVE");
   }
-  if (neust == 0) {
+  if (neust == 0)
+  {
     mode = "NEUTRAL";
     Serial.println("Mode swithched to : NUETRAL");
   }
-  if (revst == 0) {
+  if (revst == 0)
+  {
     mode = "REVERSE";
     Serial.println("Mode swithched to : REVERSE");
   }
 
   Serial.println("CURRENT MODE IS:" + mode);
 
-
   float rpm = 0.0;
   float volt = 0.0;
-  float current = 0.0;
+  float amps = 0.0;
   float power = 0.0;
   float duty = 0.0;
 
-  if (UART.getVescValues() ) {
+  // call this function if u wanna get data
+  if (UART.getVescValues())
+  {
 
     rpm = UART.data.rpm;
     volt = UART.data.inpVoltage;
-    current = UART.data.avgInputCurrent;
+    amps = UART.data.avgInputCurrent;
     power = UART.data.inpVoltage * UART.data.avgInputCurrent;
     duty = UART.data.dutyCycleNow;
 
@@ -118,14 +175,14 @@ void loop() {
     Serial.println(duty);
     Serial.println("data given  on lcd");
 
-    lcd.setCursor(0,0);
-    lcd.print("rpm:" + rpm);
-    lcd.setCursor(0,1);
-    lcd.print("volt:" + volt);
-    lcd.setCursor(0,2);
-    lcd.print("current:" + current);
-    lcd.setCursor(0,3);
-    lcd.print("power:" + power);
+    lcd.setCursor(0, 0);
+    lcd.print("rpm:" + String(rpm));
+    lcd.setCursor(0, 1);
+    lcd.print("volt:" + String(volt));
+    lcd.setCursor(0, 2);
+    lcd.print("current:" + String(amps));
+    lcd.setCursor(0, 3);
+    lcd.print("power:" + String(power));
 
     // Serial.print("RPM: ");
     // Serial.print(UART.data.rpm);
@@ -144,25 +201,24 @@ void loop() {
     Serial.println("Failed to get data!");
   }
 
-  if (mode == "DRIVE") {
+  if (mode == "DRIVE")
+  {
     dutyset(duty, pot);
-  } else if (mode == "REVERSE") {
+  }
+  else if (mode == "REVERSE")
+  {
     dutyset(duty, pot);
-  } else if (mode == "NEUTRAL") {
+  }
+  else if (mode == "NEUTRAL")
+  {
     dutyset(duty, 0);
-  } else {
+  }
+  else
+  {
     UART.setDuty(0);
     Serial.println("idk what im doing i didnt get a mode");
   }
 
 
-
-  /** Call the function getVescValues() to acquire data from VESC */
-
   delay(500);
 }
-
-// hello my name is logan gao
-// i like trains
-// my name is kay singhal
-// i am super nonchalant
