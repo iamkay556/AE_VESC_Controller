ad4d66494af0a15049270847f0da797a70d19d25
kay singhal <130114612+iamkay556@users.noreply.github.com>
2025-04-15 00:48:12 -0700

    post full boat testing day 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,33 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
 #include <VescUart.h>
+#include <BuckPSU.h>
 #include <ezButton.h>
+// #include <SoftwareWire.h>
+// #include <hd44780.h>
+// #include <hd44780ioClass/hd44780_I2Cexp.h>  // For I2C (PCF8574)
 
 // init the switches -------> limit switches put them wherever there is space and add pin #s
 ezButton driveSwitch(49);
-ezButton neutralSwitch(51);
-ezButton reverseSwitch(53);
+ezButton neutralSwitch(53);
+ezButton reverseSwitch(51);
 
 String mode = "none";
 
 #define readPIN A0       // pot pin
-#define SignalBattery 22 // signal from buck to activate battery relay
-#define SignalSolar 23   // signal from buck to activate solar relay
+#define SignalBattery 26 // signal from buck to activate battery relay
+#define SignalSolar 28   // signal from buck to activate solar relay
 
 #define relayBattery 24 // output pin to control battery relay
 #define relaySolar 25   // output pin to control solar relay
 
 VescUart UART;
+BuckPSU psu(Serial2);
 
-LiquidCrystal_I2C lcd(0x27, 15, 4);
+// SoftwareWire softI2C(3, 2); //sda scl
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+// hd44780_I2Cexp lcd2(0x27, &softI2C);
 
 void setup()
 {
@@ -31,6 +39,7 @@ void setup()
 
   /** Setup UART port (Serial1 on Atmega32u4) */
   Serial1.begin(115200);
+  Serial2.begin(4800);
 
   while (!Serial)
   {
@@ -47,8 +56,8 @@ void setup()
 
   pinMode(readPIN, INPUT);
 
-  pinMode(SignalBattery, INPUT);
-  pinMode(SignalSolar, INPUT);
+  pinMode(SignalBattery, INPUT_PULLUP);
+  pinMode(SignalSolar, INPUT_PULLUP);
 
   pinMode(relayBattery, OUTPUT);
   pinMode(relaySolar, OUTPUT);
@@ -60,6 +69,8 @@ void setup()
   driveSwitch.setDebounceTime(10);
   neutralSwitch.setDebounceTime(10);
   reverseSwitch.setDebounceTime(10);
+
+  lcd.clear();
 
   delay(1000);
 }
@@ -97,13 +108,39 @@ void dutyset(float start, float end)
   }
 }
 
+void buckset(float start, float end)
+{
+  const float step = 1000;
+  const float tolerance = 500;
+
+  while (abs(start - end) > tolerance)
+  {
+    if (end > start)
+    {
+      start += step;
+      if (start > end)
+        start = end;
+    }
+    else if (end < start)
+    {
+      start -= step;
+      if (start < end)
+        start = end;
+    }
+
+    psu.setVoltageMilliVolts(start);
+
+    delay(10);
+  }
+}
+
 void loop()
 { // --------------------------------- LOOP ---------------------------------
 
   int batonoff = digitalRead(SignalBattery);
   int solonoff = digitalRead(SignalSolar);
 
-  if (batonoff == 1)
+  if (batonoff == 0)
   {
     digitalWrite(relayBattery, HIGH);
     Serial.println("BATTERY ON");
@@ -113,15 +150,21 @@ void loop()
     digitalWrite(relayBattery, LOW);
     Serial.println("BATTERY OFF");
   }
-  if (solonoff == 1)
+  if (solonoff == 0)
   {
     digitalWrite(relaySolar, HIGH);
     Serial.println("SOLAR ON");
+
+    buckset(0, 28000);
+    psu.enableOutput(true);
   }
   else
   {
     digitalWrite(relaySolar, LOW);
     Serial.println("SOLAR OFF");
+
+    buckset(28000, 0);
+    psu.enableOutput(false);
   }
 
   driveSwitch.loop();
@@ -135,31 +178,34 @@ void loop()
   /** Read The Input from the petentiometer and map the values from 0 - 1*/
   float pot = analogRead(readPIN);
   pot = (pot / 1023) * 1.0;
-  Serial.println(pot);
+  Serial.print("Pot: ");
+  Serial.print(pot);
+  Serial.print(", ");
 
   if (drist == 0)
   {
     mode = "DRIVE";
-    Serial.println("Mode swithched to : DRIVE");
   }
   if (neust == 0)
   {
     mode = "NEUTRAL";
-    Serial.println("Mode swithched to : NUETRAL");
   }
   if (revst == 0)
   {
     mode = "REVERSE";
-    Serial.println("Mode swithched to : REVERSE");
   }
-
-  Serial.println("CURRENT MODE IS:" + mode);
+  Serial.print("Current Mode: ");
+  Serial.print(mode);
+  Serial.print(", ");
 
   float rpm = 0.0;
   float volt = 0.0;
   float amps = 0.0;
   float power = 0.0;
   float duty = 0.0;
+
+  lcd.clear();
+  // lcd.print("Hi");
 
   // call this function if u wanna get data
   if (UART.getVescValues())
@@ -175,14 +221,20 @@ void loop()
     Serial.println(duty);
     Serial.println("data given  on lcd");
 
+    String voltage = "V: " + String((int)volt);
+    String current = " C: " + String(amps);
+    String pow = "P: " + String((int)power);
+    String dut = " D: " + String(duty);
+
     lcd.setCursor(0, 0);
-    lcd.print("rpm:" + String(rpm));
+    lcd.print(voltage);
+    int col = sizeof(voltage) / sizeof(String);
+    lcd.setCursor(6, 0);
+    lcd.print(current);
     lcd.setCursor(0, 1);
-    lcd.print("volt:" + String(volt));
-    lcd.setCursor(0, 2);
-    lcd.print("current:" + String(amps));
-    lcd.setCursor(0, 3);
-    lcd.print("power:" + String(power));
+    lcd.print(pow);
+    lcd.setCursor(6, 1);
+    lcd.print(dut);
 
     // Serial.print("RPM: ");
     // Serial.print(UART.data.rpm);
@@ -219,6 +271,5 @@ void loop()
     Serial.println("idk what im doing i didnt get a mode");
   }
 
-
-  delay(500);
+  delay(50);
 }
